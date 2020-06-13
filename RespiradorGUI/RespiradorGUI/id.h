3e2eba070aa839eec23f2eed61d74b0501eeebda
Abraham Meza <abraham.meza@sciodesk.com>
2020-06-12 21:36:47 -0500

    creada enumeracion de id para manejar eventos

---
+++ RespiradorGUI/RespiradorGUI/id.h
@@ -0,0 +1,12 @@
+#pragma once
+namespace window
+{
+	enum id
+	{
+		MAINWINDOW = wxID_HIGHEST + 1,
+		MENU_QUIT = wxID_EXIT,
+		MENU_ABOUT = wxID_ABOUT,
+		START_BUTTON = 10000,
+		STOP_BUTTON = 10001
+	};
+}
